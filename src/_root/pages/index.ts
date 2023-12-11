7a325cca6edbefd9730006d608313f283b5f4fa4
andrei <dyausinoh@hotmail.com>
2023-12-11 14:52:42 -0500

    snapgram-app-first-commit

---
+++ src/_root/pages/index.ts
@@ -0,0 +1,10 @@
+
+export * from './Home';
+export * from './Explore';
+export * from './AllUsers';
+export * from './EditPost';
+export * from './PostDetails';
+export * from './Profile';
+export * from './Saved';
+export * from './UpdateProfile';
+export * from './CreatePost';
