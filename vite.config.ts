39758981af9ea54016c234ec9ce3bc76fa2498c5
andrei <dyausinoh@hotmail.com>
2023-12-11 19:32:56 -0500

    api proxy added

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
  
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'https://cloud.appwrite.io/v1'
+    }
+  },
+  build: {
+    manifest: true,
+  },
   plugins: [react()],
   resolve: {
     alias: {
