5b6881c419afbf461c1667f2d6b763f7ad2cca20
andrei <dyausinoh@hotmail.com>
2023-12-13 03:46:55 -0500

    validation tags updated

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -7,6 +7,7 @@ export const SignupValidation = z.object({
     email: z.string().email(),
     password: z.string().min(8, {message: 'Password must be at least 8 characters'})
 });
+
 export const SigninValidation = z.object({
     email: z.string().email(),
     password: z.string().min(8, {message: 'Password must be at least 8 characters'})
@@ -16,12 +17,13 @@ export const PostValidation = z.object({
     caption: z.string().min(5).max(2000),
     file: z.custom<File[]>(),
     location: z.string().min(2).max(100),
-    tags: z.string().refine((value) => /^[a-zA-Z0-9]+(?:, |,)?[a-zA-Z0-9]+$/.test(value), 'Tags should only contain letters and numbers, separated by a comma'),
+    tags: z.string().refine((value) => /^([a-zA-Z]+\s*,\s*)+[a-zA-Z]+$/.test(value), 'Tags should only contain letters and numbers, separated by a comma'),
 });
+
 export const ProfileValidation = z.object({
     file: z.custom<File[]>(),
     name: z.string().min(2, { message: "Name must be at least 2 characters." }),
     username: z.string().min(2, { message: "Name must be at least 2 characters." }),
     email: z.string().email(),
     bio: z.string(),
-  });
+});
